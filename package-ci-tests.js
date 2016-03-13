402cc4055d412c7640dcd8121c0218ee72fa8f2d
Rongky <rongkyr3@yahoo.com>
2016-03-14 01:56:56 +0800

    Committing .eslintrc and related alterations.

--- package-ci-tests.js
+++ package-ci-tests.js
@@ -1,5 +1,35 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
+/**
+ * @fileOverview The unit tests for "yourpackage".
+ * @author Your Self <yourself.yourorg@gmail.com>
+ * @version v0.0.1
+ * @license MIT
+ */
+
+/**
+ * Tinytest unit tests
+ * @namespace Tinytest
+ */
+
+/**
+ * Simply verifies that true equals true
+ * @name sanityCheckEQ
+ * @memberof Tinytest
+ * @function
+ * @param  test {Tinytest} Check Equality
+ * @return {None}
+ */
+Tinytest.add('Check Equality', function sanityCheckEQ(test) {
   test.equal(true, true);
+});
+
+/**
+ * Simply verifies that false doesn't equals true
+ * @name sanityCheckNEQ
+ * @memberof Tinytest
+ * @function
+ * @param  test {Tinytest} Check Inequality
+ * @return {None}
+ */
+Tinytest.add('Check inequality', function sanityCheckNEQ(test) {
+  test.notEqual(true, false);
 });
